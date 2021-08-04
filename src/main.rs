e6d22ceb634a0a1d242f4c83f78d664ca7816948
kimhoon <kimhoon0316@gmail.com>
2021-08-04 15:58:11 +0900

    Chap.1 ing

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}", self.real, self.imag)
+    }
+}
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // index, value
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+fn main() {
+    let minmax = MinMax(0, 14);
+
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big ragne is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let complex = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
