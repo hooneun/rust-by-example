225cf222e4220b43fb1601ffdbebff82d3ba0bdf
kimhoon <kimhoon0316@gmail.com>
2021-08-05 17:33:37 +0900

    array, slice

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,44 @@
-use core::fmt;
-use std::fmt::{Display, Formatter};
+use std::mem;
 
-fn reverse(pair: &(f32, f32, f32, f32)) -> (f32, f32, f32, f32) {
-    (pair.0, pair.1, pair.2, pair.3)
-}
-
-fn transpose(matrix: Matrix) -> Matrix {
-    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
-}
-
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
-
-impl Display for Matrix {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "( {} {} ) \n( {} {} )", self.0, self.1, self.2, self.3)
-    }
+fn analyze_slice(slice: &[i32]) {
+    println!("전달된 슬라이스의 첫 요소는 {}입니다.", slice[0]);
+    println!(
+        "전달된 슬라이스는 {}개의 요소를 가지고 있습니다.",
+        slice.len()
+    );
 }
 
 fn main() {
-    let long_tuple = (
-        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    // 길이가 고정된 배열
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // 모든 요소들을 같은 값으로 초기화
+    let ys: [i32; 500] = [0; 500];
+
+    println!("배열의 첫번째 요소: {}", xs[0]);
+    println!("배열의 두번째 요소: {}", xs[1]);
+    println!("배열의 요소의 갯수: {}", xs.len());
+
+    println!("ys 배열의 첫번째 요소: {}", ys[0]);
+    println!("ys 배열의 두번째 요소: {}", ys[1]);
+    println!("ys 배열의 요소의 갯수: {}", ys.len());
+
+    println!(
+        "배열에는 {} 바이트가 할당되어 있습니다.",
+        mem::size_of_val(&xs)
     );
 
-    println!("long tuple first value: {}", long_tuple.0);
-    println!("long tuple second value: {}", long_tuple.1);
+    // 배열은 자동으로 슬라이스 형태로 빌릴수있다.
+    println!("배열 전체를 슬라이스로 빌립니다.");
+    analyze_slice(&xs);
 
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
-    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    // println!("too long tuple: {:?}", too_long_tuple);
-    //
-    let pair = (1, true);
-    println!("pair is {:?}", pair);
+    // 슬라이스느 배열의 특정 부분을 지정할 수 있습니다.
+    // 문법은 [starting_index..ending_index] 입니다.
+    // starting_index 는 슬라이스의 처음을 나타내고
+    // ending_index 는 슬라이스의 마지막 위치를 나타냅니다.
+    println!("배열의 일부를 슬라이스로 빌립니다");
+    analyze_slice(&ys[1..4]);
 
-    //    println!("the reversed is pair is {:?}", reverse(pair));
-
-    println!("one element tuple: {:?}", (5u32,));
-    println!("just an integer: {:?}", (5u32));
-
-    let tuple = (1, "hello", 4.5, true);
-
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
-
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
-
-    println!("{}", matrix);
-
-    println!("Matrix:\n{}", matrix);
-    println!("Transpose:\n{}", transpose(matrix));
+    // 아래 문장에서는 index out of bounds 오류가 발생합니다.
+    println!("{}", ys[5]);
 }
