a7a0a41930d3735db1d7032a9a92f5464c92625a
kimhoon <kimhoon0316@gmail.com>
2021-08-06 18:36:47 +0900

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,54 @@
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Paste(String),
+    Click { x: i64, y: i64 },
+}
+
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        }
+    }
+}
+
 #[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
 }
 
-struct Unit;
-
-struct Pair(i32, f32);
-
-#[derive(Debug)]
-struct Point {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    top_left: Point,
-    bottom_right: Point,
-}
-
-fn rect_area(rect: Rectangle) -> f32 {
-    rect.top_left.y * rect.bottom_right.x
-}
-
-fn square(p: Point, f: f32) -> Rectangle {
-    Rectangle {
-        top_left: Point { y: f, x: 0.0 },
-        bottom_right: Point { x: f, ..p },
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
     }
 }
 
 fn main() {
-    let name = String::from("Peter");
-    let age = 27;
-    let peter = Person { name, age };
+    let pressed = WebEvent::KeyPress('x');
+    let pasted = WebEvent::Paste("my text".to_string());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
 
-    println!("{:?}", peter);
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
 
-    let point: Point = Point { x: 10.3, y: 0.4 };
-    println!("point coordinates: ({}, {})", point.x, point.y);
+    let x = VeryVerboseEnumOfThingsToDoWithNumbers::Add;
+    let y = VeryVerboseEnumOfThingsToDoWithNumbers::Subtract;
 
-    let bottom_right = Point { x: 5.2, ..point };
-    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
-
-    let Point {
-        x: left_edge,
-        y: top_edge,
-    } = point;
-
-    let rectangle = Rectangle {
-        top_left: Point {
-            x: left_edge,
-            y: top_edge,
-        },
-        bottom_right: bottom_right,
-    };
-
-    println!("Rect area: {}", rect_area(rectangle));
-
-    let _unit = Unit;
-    let pair = Pair(1, 0.1);
-
-    println!("pair contains {:?} and {:?}", pair.0, pair.1);
-
-    let Pair(integer, decimal) = pair;
-
-    println!("pair contains {:?} and {:?}", integer, decimal);
-
-    println!("square: {:?}", square(point, 4.0));
+    println!("{}", x.run(1, 2));
+    println!("{}", y.run(3, 2));
 }
