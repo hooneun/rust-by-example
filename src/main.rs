92c3b0fb5253838539a6eb8556e3f20dbb21ce09
kimhoon <kimhoon0316@gmail.com>
2021-08-08 20:48:08 +0900

    type casting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 fn main() {
-    let mut mutable_integer = 7i32;
+    let decimal = 65.4321_f32;
 
-    {
-        let mutable_integer = mutable_integer;
+    //let integer: u8 = decimal;
+    //
+    let integer = decimal as u8;
+    let character = integer as char;
 
-        println!("{}", mutable_integer);
-    }
+    //let character = decimal as char;
 
-    println!("{}", mutable_integer);
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
 
-    mutable_integer = 3;
+    println!("1000 as a u16 is: {}", 1000 as u16);
 
-    println!("{}", mutable_integer);
+    //println!("1000 as a u8 is : {}", 1000 as u8);
+
+    println!("  -1 as a u8 is : {}", (-1i8) as u8);
+
+    println!("1000 mod 256 is : {}", 1000 % 256);
+    //println!(" 232 as a i8 is : {}", 232 as i8);
+    //
+    println!("300.0 is {}", 300.0_f32 as u8);
+    println!("-100.0 as u8 is {}", -100.0_f32 as u8);
+    println!("nan as u8 is {}", f32::NAN as u8);
 }
