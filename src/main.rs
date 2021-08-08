4d6026d6b566acccd8def4e336dd7c912ae70334
kimhoon <kimhoon0316@gmail.com>
2021-08-08 20:56:34 +0900

    Inference

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 fn main() {
-    let x = 1u8;
-    let y = 2u32;
-    let z = 3f32;
+    let elem = 5u8;
 
-    let i = 1;
-    let f = 1.0;
+    let mut vec = Vec::new();
 
-    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
-    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
-    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
-    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
-    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+    vec.push(elem);
+
+    println!("{:?}", vec);
 }
