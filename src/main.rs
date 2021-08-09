88a0b7a74eac0208140d615e2814318768e1d624
kimhoon <kimhoon0316@gmail.com>
2021-08-09 19:28:38 +0900

    for and range

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 fn main() {
-    let mut n = 1;
+    //    let mut n = 1;
+    //
+    //    while n < 101 {
+    //        if n % 15 == 0 {
+    //            println!("fizzbuzz");
+    //        } else if n % 3 == 0 {
+    //            println!("fizz");
+    //        } else if n % 5 == 0 {
+    //            println!("buzz");
+    //        } else {
+    //            println!("{}", n);
+    //        }
+    //
+    //        n += 1;
+    //    }
 
-    while n < 101 {
-        if n % 15 == 0 {
+    for i in 1..=100 {
+        if i % 15 == 0 {
             println!("fizzbuzz");
-        } else if n % 3 == 0 {
+        } else if i % 3 == 0 {
             println!("fizz");
-        } else if n % 5 == 0 {
+        } else if i % 5 == 0 {
             println!("buzz");
         } else {
-            println!("{}", n);
+            println!("{}", i);
         }
-
-        n += 1;
     }
 }
