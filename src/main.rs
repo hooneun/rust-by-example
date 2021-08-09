38a77e543980ff523e6e5026130a74540eb314c2
kimhoon <kimhoon0316@gmail.com>
2021-08-09 12:07:47 +0900

    expression

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use std::fmt;
-
-struct Circle {
-    radius: i32,
-}
-
-impl fmt::Display for Circle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Circle of radius {}", self.radius)
-    }
-}
-
 fn main() {
-    let circle = Circle { radius: 6 };
-    println!("{}", circle.to_string());
+    let x = 5u32;
 
-    let parsed: i32 = "5".parse().unwrap();
-    let turbo_parsed = "10".parse::<i32>().unwrap();
+    let y = {
+        //25
+        let x_squared = x * x;
+        // 25 * 5 = 125
+        let x_cube = x_squared * x;
 
-    let sum = parsed + turbo_parsed;
-    println!("Sum: {:?}", sum);
+        // 25 * 125 + 5
+        x_cube * x_squared + x
+    };
+
+    let z = { 2 * x };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
 }
