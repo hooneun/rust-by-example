413faf8334775d47b748e0761fff39bcb7e5cf1f
kimhoon <kimhoon0316@gmail.com>
2021-08-09 16:58:09 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 fn main() {
-    let n = 10;
+    let mut count = 0u32;
 
-    if n < 0 {
-        println!("{} is negative", n);
-    } else if n > 0 {
-        println!("{} is positive", n);
-    } else {
-        println!("{} is zero", n);
+    println!("Let's count until infinity!");
+
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+            continue;
+        }
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            break;
+        }
     }
-
-    let big_n = if n < 10 && n > -10 {
-        println!(", and is a small number, increase ten-fold");
-
-        10 * n
-    } else {
-        println!(", and is a big number, halve the number");
-
-        n / 2
-    };
-
-    println!("{} -> {}", n, big_n);
 }
