143cd3d522924dab525609a2251fc31dcb0c9771
kimhoon <kimhoon0316@gmail.com>
2021-08-10 16:44:53 +0900

    match enums

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
 fn main() {
-    let triple = (4, -2, 3);
+    let color = Color::RGB(122, 17, 40);
 
-    println!("Tell me about {:?}", triple);
+    println!("What color is it?");
 
-    match triple {
-        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
-        (1, ..) => println!("First is `1` and the rest doesn't matter"),
-        _ => println!("It doesn't matter what they are"),
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
+        ),
     }
 }
